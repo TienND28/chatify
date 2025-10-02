23a8c3e001af5cd0c5b7765a7de8877f07c54093
TienND28 <dinhtiensw28@gmail.com>
2025-10-02 17:41:24 +0700

    cleaned

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,11 +1,13 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
-   if (!process.env.JWT_SECRET) {
+  const {JWT_SECRET, NODE_ENV} = process.env;
+
+   if (!JWT_SECRET) {
     throw new Error("JWT_SECRET environment variable is not configured");
   }
   
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+  const token = jwt.sign({ userId }, JWT_SECRET, {
     expiresIn: "7d",
   });
 
@@ -13,7 +15,7 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // preven Xss attacks: cross-site scripting
     sameSize: "strict", // CSRF attacks
-    secure: process.env.NODE_ENV === "development" ? false : true,
+    secure: NODE_ENV === "development" ? false : true,
   });
 
   return token;
